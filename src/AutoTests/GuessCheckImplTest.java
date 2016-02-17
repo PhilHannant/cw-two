29f5dcd3319c139bd9e13752e4623aadb90d4d0a
philhannant <philip.a.hannant@gmail.com>
2016-02-17 18:45:44 +0000

    correctGuessCheck working

--- src/AutoTests/GuessCheckImplTest.java
+++ src/AutoTests/GuessCheckImplTest.java
@@ -1,6 +1,8 @@
 package AutoTests;
 
-import org.junit.*;
+import Implementation.GuessCheckImpl;
+import org.junit.Test;
+
 
 import static org.junit.Assert.*;
 
@@ -9,8 +11,14 @@ import static org.junit.Assert.*;
  */
 public class GuessCheckImplTest {
 
-    @org.junit.Test
-    public void testCheckGuess() throws Exception {
-
+    @Test
+    public void testCheckCorrectGuess() {
+        GuessCheckImpl gc = new GuessCheckImpl();
+        char[] inputAnswer = {'r', 'g', 'b', 'o'};
+        char[] inputCode = {'r', 'g', 'b', 'o'};
+        int[] expected = {0, 4};
+        int[] output = gc.CheckGuess(inputAnswer, inputCode);
+        assertEquals(expected[1], output[1]);
+        assertEquals(expected[0], output[0]);
     }
 }
