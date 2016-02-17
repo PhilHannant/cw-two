d0fe38f9abc481346092fb79b7a8899da633eea5
philhannant <philip.a.hannant@gmail.com>
2016-02-17 18:18:45 +0000

    all tests working

--- src/AutoTests/InCheckImplTest.java
+++ src/AutoTests/InCheckImplTest.java
@@ -3,6 +3,7 @@ package AutoTests;
 import Implementation.InCheckImpl;
 import Interfaces.InputErrorChecker;
 import org.junit.*;
+import org.junit.Test;
 
 import static org.junit.Assert.*;
 
@@ -15,6 +16,30 @@ public class InCheckImplTest {
     public void testErrorCheckFalse() throws Exception {
         InputErrorChecker inEr = new InCheckImpl();
         char[] input = {'f', 'g', 'r', 'b'};
+        boolean check = false;
+        assertEquals(check, inEr.ErrorCheck(input));
+    }
+
+    @Test
+    public void testErrorCheckTrue() throws Exception {
+        InputErrorChecker inEr = new InCheckImpl();
+        char[] input = {'g', 'g', 'r', 'b'};
+        boolean check = true;
+        assertEquals(check, inEr.ErrorCheck(input));
+    }
+
+    @Test
+    public void testErrorCheckSize() throws Exception {
+        InputErrorChecker inEr = new InCheckImpl();
+        char[] input = {'g', 'g', 'r', 'b', 'g', 'r'};
+        boolean check = false;
+        assertEquals(check, inEr.ErrorCheck(input));
+    }
+
+    @Test
+    public void testEmpty(){
+        InputErrorChecker inEr = new InCheckImpl();
+        char[] input = new char[1];
         boolean check = false;
         assertEquals(check, inEr.ErrorCheck(input));
     }
