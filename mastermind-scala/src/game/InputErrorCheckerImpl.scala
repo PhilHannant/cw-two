48b41f07097867ee15b5653768d7ffbadc4f5118
A. Cole and P. Hannant <philip.a.hannant@gmail.com>
2016-04-08 17:25:55 +0100

    errorCheck added

--- mastermind-scala/src/game/InputErrorCheckerImpl.scala
+++ mastermind-scala/src/game/InputErrorCheckerImpl.scala
@@ -1,6 +1,11 @@
 package game
 import traits._
 
-class InputErrorCheckerImpl extends InputErrorChecker{
-  override def ErrorCheck(input: Array[Colours]): Boolean = ???
+class InputErrorCheckerImpl extends InputErrorChecker with Values{
+  override def ErrorCheck(input: Array[Colours]): Boolean = {
+    var res: Boolean = true
+    input.foreach(n => if(!(CharMap contains (n))) res = false)
+    if(input.length != codeLength) res = false
+    res
+  }
 }
